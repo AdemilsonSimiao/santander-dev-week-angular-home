3be6e209d0e3dcaf6976c7265a96452755477969
AdemilsonSimiao <biassistencia@gmail.com>
2023-07-25 20:52:44 -0300

    atualizacao do card

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountDataModel } from 'src/app/model/accountDatamodel';
+import { CardsService } from 'src/app/services/cards.service';
 
 @Component({
   selector: 'app-card-box',
   templateUrl: './card-box.component.html',
   styleUrls: ['./card-box.component.css']
 })
-export class CardBoxComponent {
+export class CardBoxComponent implements OnInit{
+  constructor(private service: CardsService){
 
+  }
+  accountData: AccountDataModel = {
+    name: "João",
+    account: {
+        agency: "0000",
+        number: "00.00000-0"
+    },
+    card: {
+        limit: 0,
+        number: "0000"
+    }
+  };
+  ngOnInit(): void {
+      this.getAccountData();
+  }
+  getAccountData(){
+    this.service.getCard().subscribe(data =>{
+      this.accountData.name = data.name
+      this.accountData.account.agency = data.account.agency
+      this.accountData.account.number = data.account.number
+      this.accountData.card.limit = data.card.limit
+      this.accountData.card.number = data.card.number
+    })
+  }
 }
